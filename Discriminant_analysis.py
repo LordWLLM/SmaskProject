c6d42866d4c951510bb037506f3d071ab63f1408
LordWLLM <ville.gustav@protonmail.com>
2025-01-26 18:22:54 +0100

    added DiscAnal model file

---
+++ Discriminant_analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+import sklearn.preprocessing as skl_pre
+#import sklearn.linear_model as skl_lm
+import sklearn.discriminant_analysis as skl_da
+#import sklearn.neighbors as skl_nb
+#import sklearn.model_selection as skl_ms
--- LogisticalRegression.py
+++ LogisticalRegression.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-#import sklearn.preprocessing as skl_pre
+import sklearn.preprocessing as skl_pre
 import sklearn.linear_model as skl_lm
 #import sklearn.discriminant_analysis as skl_da
 #import sklearn.neighbors as skl_nb
